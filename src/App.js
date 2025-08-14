3eb1fea28ebea0b0b74094532b14283dc967f835
Ramanathan20032 <154827095+Ramanathan20032@users.noreply.github.com>
2025-08-14 10:30:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ScrollToTop />
+      <div className="App">
+        <HelmetProvider>
+          <Routes>  
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </HelmetProvider>
+      </div>
+    </Router>
   );
 }
 
